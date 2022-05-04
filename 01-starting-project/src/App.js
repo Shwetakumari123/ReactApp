1e1a48ef4145a434956bccdd9f5eaf5403da6d94
shweta <shweta@iiitmanipur.ac.in>
2022-05-04 16:45:34 +0530

    Use Reducer

---
+++ 01-starting-project/src/App.js
@@ -0,0 +1,18 @@
+
+import {Fragment} from 'react'
+import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals'
+function App() {
+  return (
+    <Fragment>
+      <Header/>
+      
+        <Meals/>
+     
+
+    </Fragment>
+    
+  );
+}
+
+export default App;
